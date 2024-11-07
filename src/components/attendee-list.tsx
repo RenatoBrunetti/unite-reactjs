1338209a1ddfb077c011a964dcccd41bb63e1130
Renato Brunetti <recarapia@gmail.com>
2024-11-06 22:21:47 -0300

    feat: add attendees list pagination

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { formatDistance } from "date-fns";
 import { enUS } from "date-fns/locale";
 import {
@@ -18,11 +18,53 @@ import { TableRow } from "./table/table-row";
 
 import { attendees } from "../data/attendees";
 
-export function AttendeeList() {
-  const [search, setSearch] = useState("");
+const offset = 10;
 
+export function AttendeeList() {
+  const hasPageBeenRendered = useRef(false);
+  const [search, setSearch] = useState("");
+  const [pagination, setPagination] = useState(1);
+  const [minPaginationValues, setMinPaginationValues] = useState(1);
+  const [maxPaginationValues, setMaxPaginationValues] = useState(offset);
+
+  const lastPage = Math.ceil(attendees.length / offset);
+
+  useEffect(() => {
+    if (hasPageBeenRendered.current) {
+      setMinPaginationValues((pagination - 1) * offset);
+      setMaxPaginationValues(pagination * offset);
+    }
+    hasPageBeenRendered.current = true;
+  }, [pagination]);
+
+  // Search
   function onSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
+  }
+
+  // Pagination
+  function goToFirstPage() {
+    if (pagination > 1) {
+      setPagination(1);
+    }
+  }
+
+  function goToPreviousPage() {
+    if (pagination > 1) {
+      setPagination((current) => current - 1);
+    }
+  }
+
+  function goToNextPage() {
+    if (pagination < lastPage) {
+      setPagination((current) => current + 1);
+    }
+  }
+
+  function goToLastPage() {
+    if (pagination < lastPage) {
+      setPagination(lastPage);
+    }
   }
 
   return (
@@ -58,68 +100,88 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {attendees.map((attendee) => (
-            <TableRow
-              key={attendee.id}
-              className="border-b border-white/10 hover:bg-white/5"
-            >
-              <TableCell>
-                <input
-                  type="checkbox"
-                  className="size-4 bg-black/20 rounded border border-white/10"
-                />
-              </TableCell>
-              <TableCell>{attendee.id}</TableCell>
-              <TableCell>
-                <div className="flex flex-col gap-1">
-                  <span className="font-semibold text-white">
-                    {attendee.name}
-                  </span>
-                  <span>{attendee.email}</span>
-                </div>
-              </TableCell>
-              <TableCell>
-                {formatDistance(attendee.createdAt, new Date(), {
-                  locale: enUS,
-                  addSuffix: true,
-                })}
-              </TableCell>
-              <TableCell>
-                {formatDistance(attendee.checkedInAt, new Date(), {
-                  locale: enUS,
-                  addSuffix: true,
-                })}
-              </TableCell>
-              <TableCell>
-                <IconButton transparent>
-                  <MoreHorizontal className="size-4" />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          ))}
+          {attendees
+            .slice(minPaginationValues, maxPaginationValues)
+            .map((attendee) => (
+              <TableRow
+                key={attendee.id}
+                className="border-b border-white/10 hover:bg-white/5"
+              >
+                <TableCell>
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border border-white/10"
+                  />
+                </TableCell>
+                <TableCell>{attendee.code}</TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">
+                      {attendee.name}
+                    </span>
+                    <span>{attendee.email}</span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {formatDistance(attendee.createdAt, new Date(), {
+                    locale: enUS,
+                    addSuffix: true,
+                  })}
+                </TableCell>
+                <TableCell>
+                  {formatDistance(attendee.checkedInAt, new Date(), {
+                    locale: enUS,
+                    addSuffix: true,
+                  })}
+                </TableCell>
+                <TableCell>
+                  <IconButton transparent>
+                    <MoreHorizontal className="size-4" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
         </tbody>
         <tfoot>
           <tr>
             <TableCell className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-              Showing 10 of 52 items
+              Showing 10 of {attendees.length} items
             </TableCell>
             <td
               className="py-3 px-4 text-sm text-zinc-300 text-right"
               colSpan={3}
             >
               <div className="inline-flex items-center gap-8">
-                <span>Page 1 of 6</span>
+                <span>
+                  Page {pagination} of {lastPage}
+                </span>
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton
+                    onClick={goToFirstPage}
+                    disabled={pagination === 1}
+                    transparent={pagination === 1}
+                  >
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToPreviousPage}
+                    disabled={pagination === 1}
+                    transparent={pagination === 1}
+                  >
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={pagination === lastPage}
+                    transparent={pagination === lastPage}
+                  >
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={pagination === lastPage}
+                    transparent={pagination === lastPage}
+                  >
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
